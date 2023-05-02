ba522742197216e1da5bcce4ffaf85762c29d51f
Rafsanfaruk <39384467+Rafsanfaruk@users.noreply.github.com>
2023-05-02 19:46:00 +0600

    data client site working

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
-// const cors = require("cors");
-// const port = process.env.PORT || 5000;
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Welcome server');
@@ -9,23 +12,13 @@ app.get('/', (req, res) => {
 
 const recipeData =require('./data.json');
 
-app.get("/card", (req, res) => {
+app.get("/data", (req, res) => {
     res.send(recipeData);
   });
 
-  app.get("/card/:id", (req, res) => {
-    const id = parseInt(req.params.id);
-    console.log(id);
-  
-    // if (id === 0) {
-    //   res.send(recipeData);
-    // } else {
-    //   const recipeDataAll = recipeData.filter((n) => parseInt(n.recipeData_id) === id);
-    //   res.send(recipeDataAll);
-    // }
-});
 
-app.listen(5000,()=>{
-    console.log('server is  running on port');
+
+app.listen(port,()=>{
+    console.log(`Recipe Api is running on ${port}`);
 })
 
